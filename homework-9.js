1622251725f96154364b0b768ceb87ec4b4a83ec
vllayar <420allayar@gmail.com>
2026-08-15 20:14:36 +0500

    finished task 3

--- homework-9.js
+++ homework-9.js
@@ -2,3 +2,8 @@
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const newNumbers = numbers.slice(4, 10);
 console.log(`Task 2\n${newNumbers}`);
+
+// TASK 3
+const drinks = ['tea', 'coffee', 'juice', 'milk', 'lemonade', 'cocktail'];
+console.log(drinks.includes('coffee'));
+console.log(drinks.includes('wine'));
